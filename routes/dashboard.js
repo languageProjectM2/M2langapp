8aa80ef84146badb199f57f6bfb58aff72279782
Nico Cmt <comotti.n@gmail.com>
2021-02-18 13:20:38 +0100

    add working router / still need to find a perfectly matching english words json

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,38 +18,38 @@ router.get("/word-search", (req, res, next) => {
   // console.log("req.session.user: ", req.session.user);
   axios
     .get(
-      "https://raw.githubusercontent.com/RazorSh4rk/random-word-api/master/words.json"
+      //"https://raw.githubusercontent.com/RazorSh4rk/random-word-api/master/words.json"
+      ""
     )
     .then((response) => {
       const randomIndex = Math.floor(Math.random() * response.data.length)
+      //console.log("RANDOM INDEX LOOK LOOK LOOK" , randomIndex)
       const randomWord = response.data[randomIndex]
+      console.log("RANDOM WORD LOOK LOOK LOOK" , response.data[4])
       //console.log('fuck', response.data)
-      console.log(randomWord)
-      return(randomWord)
-    })
-  axios
-    .get(
-      `https://od-api.oxforddictionaries.com/api/v2/translations/en/de/${randomWord || "Error"}?strictMatch=false`
-      )
-    .then((picked) => {
-      const searchedWord = picked.data.results[0].id;
-      const translatedWord = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text
-      const engSentence = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].translations[0].text
-      const gerSentence = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text
-      const ipaWord = picked.data.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling
-      res.render("search-results", { searchedWord, translatedWord, gerSentence, engSentence, ipaWord, randomWord });
-    })
+      //console.log('RANDOM WORD IS HEEEEEERRRRRRRRREEEEE', randomWord)
+     axios
+        .get(
+          `https://od-api.oxforddictionaries.com/api/v2/translations/en/de/${randomWord || "error"}?strictMatch=false` 
+        )
+          .then((picked) => {
+            //console.log('AND THIS IS PICKED', picked)
+            const searchedWord = picked.data.results[0].id;
+            const translatedWord = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text
+            const engSentence = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].translations[0].text
+            const gerSentence = picked.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text
+            const ipaWord = picked.data.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling
+            //console.log('loook heeeerrre',{ searchedWord, translatedWord, gerSentence, engSentence, ipaWord, randomWord } )
+            res.render("search-results", { searchedWord, translatedWord, gerSentence, engSentence, ipaWord, randomWord });
+          })
+        .catch(err => {
+          console.log(err)
+        })
+        })
     .catch(err => {
       console.log(err);
     })
   })
-
-// here we will add a shuffling function
-
-//function getRandomWord()
-
-
-
 
 
 
--- routes/randomWord.js
+++ routes/randomWord.js
@@ -1,34 +1,38 @@
-const router = require("express").Router();
-const axios = require('axios');
-const Word = require('../models/Word')
-const User = require('../models/User.model')
+// const router = require("express").Router();
+// const axios = require('axios');
+// const Word = require('../models/Word')
+// const User = require('../models/User.model')
 
-require("dotenv/config");
+// require("dotenv/config");
 
 
-router.get("/random-path", (req, res, next) => {
-  // console.log("req.session.user: ", req.session.user);
+// router.get("/random-path", (req, res, next) => {
+//   // console.log("req.session.user: ", req.session.user);
 
-  axios
-    .get(
-      "https://raw.githubusercontent.com/RazorSh4rk/random-word-api/master/words.json"
-    )
-    .then((response) => {
-      const randomIndex = Math.floor(Math.random() * response.data.length)
-      const randomWord = response.data[randomIndex]
-      console.log(response.data)
-      axios.get(`https://od-api.oxforddictionaries.com/api/v2/translations/en/de/${randomWord || "Error"}?strictMatch=false`)
-      const searchedWord = def.data.results[0].id;
-      const translatedWord = def.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text
-      const engSentence = def.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].translations[0].text
-      const gerSentence = def.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text
-      const ipaWord = def.data.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling
-      res.render("search-results", { searchedWord, translatedWord, gerSentence, engSentence, ipaWord, wordInput });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  })
+//   axios
+//     .get(
+//      // "https://raw.githubusercontent.com/RazorSh4rk/random-word-api/master/words.json"
+//      "https://github.com/dwyl/english-words/blob/master/words_dictionary.json"
+//     )
+//     .then((response) => {
+//       const randomIndex = Math.floor(Math.random() * response.data.length)
+//       const randomWord = response.data[randomIndex]
+//       console.log(response.data[randomIndex]) 
+//     axios
+//     .get(
+//       `https://od-api.oxforddictionaries.com/api/v2/translations/en/de/${randomWord || "Error"}?strictMatch=false`
+//     )
+//       const searchedWord = response.data.results[0].id;
+//       const translatedWord = response.data.results[0].lexicalEntries[0].entries[0].senses[0].translations[0].text
+//       const engSentence = response.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].translations[0].text
+//       const gerSentence = response.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text
+//       const ipaWord = response.data.results[0].lexicalEntries[0].entries[0].pronunciations[0].phoneticSpelling
+//       res.render("search-results", { searchedWord, translatedWord, gerSentence, engSentence, ipaWord, wordInput });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+//   })
 
-module.exports = router;
+// module.exports = router;
 
